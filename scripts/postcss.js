44a25173650eb673e8873e8e1e3c5492c123b58d
LasaleFamine <info@godev.space>
2017-12-04 23:15:02 +0100

    feat(postcss-build-dev): added build and dev command

--- scripts/postcss.js
+++ scripts/postcss.js
@@ -1,19 +1,56 @@
+const {resolve, dirname} = require('path');
 const fs = require('fs-extra');
+const glob = require('glob-fs')({gitignore: true});
+const chokidar = require('chokidar');
 
 const postcss = require('postcss');
 const postcssrc = require('postcss-load-config');
 
-const folder = './elements/**/*.pcss';
-const outputFolder = './out';
+const isDev = process.argv.find(arg => arg.includes('watch'));
+const elementArg = process.argv.find(arg => arg.includes('element='));
+const element = elementArg ? elementArg.replace('element=', '') : null;
 
-const run = async () => {
-  const files = await fs.readdir(folder);
-  files.forEach(async file => {
-    const fileContent = await fs.readFile(`${folder}/${file}`, 'utf8');
-    const {css} = await postcssrc().then(({plugins}) => postcss(plugins).process(fileContent));
-    await fs.ensureDir(outputFolder);
-    await fs.writeFile(`${outputFolder}/${file.replace('postcss', 'css')}`, css);
+const folder = element ? `./elements/${element}/**/*.pcss` : './elements/**/*.pcss';
+
+const _process = async file => {
+  const cssFile = file.replace('postcss', 'css').replace('src', 'dist');
+  const fileContent = await fs.readFile(file, 'utf8');
+  const res = await postcssrc()
+    .then(({plugins}) => postcss(plugins).process(fileContent, {from: file, to: cssFile}));
+
+  await fs.ensureDir(dirname(cssFile));
+  await fs.remove(cssFile);
+  await fs.writeFile(cssFile, res.css);
+  return true;
+};
+
+/** Build single file changed component */
+const _buildSingleFile = async pathToFile => _process(pathToFile);
+
+/** dev command */
+const dev = () => {
+  const watcher = chokidar.watch(folder, {
+    ignored: /dist/,
+    persistent: true,
+    awaitWriteFinish: {
+      stabilityThreshold: 1000,
+      pollInterval: 100
+    }
   });
-}
 
-run().catch(err => console.error(err));
+  return watcher
+    .on('add', sourcePath => console.log(`PostCSS Watcher: file ${sourcePath} has been added`))
+    .on('change', async sourcePath => console.log(`Building: ${sourcePath}`, await _buildSingleFile(sourcePath)))
+    .on('unlink', sourcePath => console.log(`PostCSS Watcher: File ${sourcePath} has been removed`));
+};
+
+/** Build command */
+const build = async () => {
+  const files = glob.readdirSync(folder);
+  files.forEach(async file => _process(file));
+};
+
+const main = () =>
+  isDev ? dev() : build().catch(err => console.error(err));
+
+main();
