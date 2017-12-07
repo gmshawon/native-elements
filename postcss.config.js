36aad23830e1f2a29a955872c2e4d76c14b8e7a1
equinusocio <astorino.mattia@gmail.com>
2017-12-07 12:46:48 +0100

    chore: Update styles and pcss config

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,8 @@ module.exports = () => ({
     'postcss-easy-import': { extensions: '.pcss' },
     'postcss-nesting': {},
     'postcss-custom-media': {},
+    'postcss-selector-not': {},
+    'postcss-discard-comments': {},
     'autoprefixer': {},
-    'postcss-discard-comments': {}
   }
 });
