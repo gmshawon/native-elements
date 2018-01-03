a3824421449930fe512043926e0f940b7caf28eb
equinusocio <astorino.mattia@gmail.com>
2018-01-04 00:48:51 +0100

    chore: Fix postcss task plugins (wip)

--- .gulp/tasks/postcss.js
+++ .gulp/tasks/postcss.js
@@ -8,33 +8,18 @@ import gulp from 'gulp';
 import pkg from '~/package.json';
 import chalk from 'chalk';
 import postcss from 'gulp-postcss';
-
-import postcssEasyImport from 'postcss-easy-import';
-import stylelint from 'stylelint';
-import postcssMixins from 'postcss-mixins';
-import postcssNesting from 'postcss-nesting';
-import postcssCustomMedia from 'postcss-custom-media';
-import postcssSelectorNot from 'postcss-selector-not';
-import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
+import rename from 'gulp-rename';
 
 
 export const pcss = (done) => {
-  const plugins = [
-    postcssEasyImport({ extensions: '.pcss' }),
-    stylelint({ configFile: '.stylelintrc' }),
-    postcssMixins(),
-    postcssNesting(),
-    postcssCustomMedia(),
-    postcssSelectorNot(),
-    autoprefixer({ browsers: ['last 1 versions', 'not ie <= 11'] }),
-    cssnano({ preset: 'advanced' })
-  ]
 
-  return gulp.src(`${pkg.paths.elements}/**/*.pcss`)
-    .pipe(postcss(plugins))
-    .pipe(gulp.dest('./elements'));
-
+  return gulp.src(`${pkg.paths.elements}/**/!(_*).pcss`, {base: process.cwd()})
+    .pipe(postcss())
+    .pipe(rename(function (path, file) {
+      path.dirname = `/elements/${file}/dist/`
+      path.extname = '.css'
+    }))
+    .pipe(gulp.dest('.'));
   done();
 }
 
--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
-module.exports = {
-  'postcss-easy-import': { extensions: '.pcss' },
-  'stylelint': { configFile: '.stylelintrc' },
-  'postcss-mixins': {},
-  'postcss-nesting': {},
-  'postcss-custom-media': {},
-  'postcss-selector-not': {},
-  'autoprefixer': { browsers: ['last 1 versions', 'not ie <= 11'] },
-  'cssnano': { preset: 'advanced' }
-}
+module.exports = (ctx) => ({
+  plugins: {
+    'postcss-easy-import': { extensions: '.pcss' },
+    'stylelint': { configFile: '.stylelintrc' },
+    'postcss-mixins': {},
+    'postcss-nesting': {},
+    'postcss-custom-media': {},
+    'postcss-selector-not': {},
+    'autoprefixer': { browsers: ['last 1 versions', 'not ie <= 11'] },
+    'cssnano': { preset: 'advanced' }
+  }
+})
