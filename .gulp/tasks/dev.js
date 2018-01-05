b0f67ec7ab88c18528709e98236818ddea474a17
Mattia Astorino <astorino.design@gmail.com>
2018-01-05 23:53:07 +0100

    chore: Update builder logs

---
+++ .gulp/tasks/dev.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+/*
+ * > Dev
+ */
+
+import gulp from 'gulp';
+import pkg from '~/package.json';
+import chalk from 'chalk';
+
+gulp.task('dev', gulp.parallel('postcss', 'serve', () => {
+  gulp.watch([pkg.paths.elements + '/ne-**/*.pcss', 'postcss.config.js'], gulp.series('postcss'))
+    .on('change', (path, stats) => {
+      const file = path.replace('elements/', '');
+      console.log(chalk.yellow('changed:'), file);
+    })
+    .on('unlink', (path, stats) => {
+      const file = path.replace('elements/', '');
+
+      console.log(file);
+    });
+}));
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,10 @@
 import gulp from 'gulp';
+import serve from './.gulp/tasks/serve';
 import gulpStats from 'gulp-stats';
 import clean from './.gulp/tasks/clean';
 import postcss from './.gulp/tasks/postcss';
 import build from './.gulp/tasks/build';
-import watch from './.gulp/tasks/watch';
+import dev from './.gulp/tasks/dev';
 
 // set default task
 export default build;
