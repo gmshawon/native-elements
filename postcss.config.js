6811144ceee9aa63377a541428be1a708d3c238c
Mattia Astorino <astorino.design@gmail.com>
2018-07-13 21:34:22 +0200

    chore: Update postcss config and plugins switching to postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,24 @@
+const BROWSERS_LIST = ['last 1 versions', 'not ie <= 11', 'not op_mini all', 'not dead', 'not < 0.5%'];
+
 module.exports = (ctx) => ({
   plugins: {
-    'postcss-easy-import': { extensions: '.pcss' },
-    'stylelint': { configFile: '.stylelintrc' },
-    'postcss-mixins': {},
-    'postcss-nesting': {},
-    'postcss-custom-media': {},
-    'postcss-selector-not': {},
-    'autoprefixer': { browsers: ['last 1 versions', 'not ie <= 11'] },
+    'stylelint': {
+      configFile: '.stylelintrc'
+    },
+    'postcss-easy-import': {
+      extensions: '.pcss',
+      plugins: [require('stylelint')()]
+    },
+    'postcss-preset-env': {
+      browsers: BROWSERS_LIST,
+      stage: 0,
+      autoprefixer: {
+        grid: true
+      },
+      insertBefore: {
+        'nesting-rules': require('postcss-mixins')()
+      }
+    },
     'cssnano': { preset: ['advanced', {
       mergeLonghand: false
     }] },
